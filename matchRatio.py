e20f9e0e71811a884885c4fbcb5aa1d660ff1b21
Somar2940 <sota2940@gmail.com>
2021-10-18 11:14:51 +0900

    ヒストグラムの比較による処理

--- matchRatio.py
+++ matchRatio.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 
-image1 = cv2.imread('img/BBS_out.jpg',-1)
-image2 = cv2.imread('img/BBS_out.jpg',-1)
-image3 = cv2.imread('img/BBS_out2.jpg',-1)
+image1 = cv2.imread('img/IMG_0636.jpg',-1)
+image2 = cv2.imread('img/IMG_0636.jpg',-1)
+image3 = cv2.imread('img/IMG_0637.jpg',-1)
 
 height, width = image1.shape[:2]
 img_size = (width, height)
@@ -13,23 +13,21 @@ image1 = cv2.resize(image1, img_size)
 image2 = cv2.resize(image2, img_size)
 image3 = cv2.resize(image3, img_size)
 
+image1_hsv = cv2.cvtColor(image1, cv2.COLOR_BGR2HSV)
+image2_hsv = cv2.cvtColor(image2, cv2.COLOR_BGR2HSV)
+image3_hsv = cv2.cvtColor(image3, cv2.COLOR_BGR2HSV)
+
 lowerHSV = (40, 0, 0)
 upperHSV = (110,150, 150)
-result = list(range(3))
 
-def Chroma(result, img, n):
-   hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-   mask = cv2.inRange(hsv, (40, 0, 0), (110,150, 150))
-   tmp = cv2.bitwise_not(img, img, mask=mask) # 元画像とマスクを合成
-   result.append(tmp)
-   cv2.imwrite("resultImages/output" + str(n) + ".jpg", tmp)
+# 画像をヒストグラム化する
+image1_hist = cv2.calcHist([image1_hsv], [0], None, [256], [0, 256])
+image2_hist = cv2.calcHist([image2_hsv], [0], None, [256], [0, 256])
+image3_hist = cv2.calcHist([image3_hsv], [0], None, [256], [0, 256])
 
+# ヒストグラムした画像を比較
+print(cv2.compareHist(image1_hist, image2_hist, 0))
+print(cv2.compareHist(image1_hist, image3_hist, 0))
 
-Chroma(result, image1, 1)
-Chroma(result, image2, 2)
-Chroma(result, image3, 3)
-
-print(np.count_nonzero(result[0] == result[1]) / image1.size)
-print(np.count_nonzero(result[0] == result[2]) / image1.size)
 # 1.0
 # 0.46696666666666664
